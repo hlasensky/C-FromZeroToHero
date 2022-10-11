d7309cbb83dd23eefc3e2c6c891529bc5b307878
hlasensky <tomas.hlasensky@seznam.cz>
2022-10-11 07:41:21 +0200

    adding phoneNumberDB with names and numbers and main soubor t9search

---
+++ firstProject/t9search.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+struct namePlusPhone
+{
+    char name[100];
+    char phone[9];
+};
+
+int readFile(char fileName[])
+{
+}
+
+int main(int argc, char *argv[])
+{
+    printf("The argument supplied is %s\n", argv[1]);
+    printf("The argument supplied is %s\n", argv[2]);
+    printf("The argument supplied is %s\n", argv[3]);
+}
