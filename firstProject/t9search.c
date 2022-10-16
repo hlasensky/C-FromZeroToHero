cfa05adc31495df2f81527d8607bb5180e5faa3f
hlasensky <tomas.hlasensky@seznam.cz>
2022-10-16 15:13:31 +0200

    adding more comments and error messages and check on input

--- firstProject/t9search.c
+++ firstProject/t9search.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-struct namePlusPhone
+struct namePlusPhone // structure for contact
 {
     char name[101];
     char phone[101];
@@ -82,7 +82,7 @@ int findByNumber(char *sequenceOfNumbers, char *phoneNumber) // function that ta
         }
         if (counterSeqOFNum == strlen(sequenceOfNumbers)) // checking if number of matching numbers is same length as sequence of numbers
         {
-            return find;
+            return 0;
         }
     }
 
@@ -96,6 +96,7 @@ int findByString(char *sequenceOfNumbers, char *costumerName) // function that t
     int counterSeqOFNum = 0;
     char *arrayOfCharsReplInt[] = {
         "+",
+        "",
         "abc",
         "def",
         "ghi",
@@ -103,33 +104,34 @@ int findByString(char *sequenceOfNumbers, char *costumerName) // function that t
         "mno",
         "pqrs",
         "tuv",
-        "wxyz"};
+        "wxyz"}; // array representing numbers in sequence of numbers (number in sequenceOfNumbers - 1 = index in array)
 
     int indexInArrayOfCharsReplInt;
     char *stringFromArrayOfCharsReplInt;
 
-    for (counterCostumerName = 0; counterCostumerName < strlen(costumerName); counterCostumerName++) // looping through name
+    for (counterCostumerName = 0; counterCostumerName < strlen(costumerName); counterCostumerName++) // looping through costumer name
     {
-        // making number from string
-        if (sequenceOfNumbers[counterSeqOFNum] == 48)
+        // making from sequence of numbers(type char) numbers(type int)
+        if (sequenceOfNumbers[counterSeqOFNum] == 48) // checking if number is zero
         {
-            indexInArrayOfCharsReplInt = 0; // index if it's zero
+            indexInArrayOfCharsReplInt = 0;
         }
         else
         {
-            indexInArrayOfCharsReplInt = sequenceOfNumbers[counterSeqOFNum] - 49; // index
+            indexInArrayOfCharsReplInt = sequenceOfNumbers[counterSeqOFNum] - 48; // checking if number is different from zero
         }
-        stringFromArrayOfCharsReplInt = arrayOfCharsReplInt[indexInArrayOfCharsReplInt]; // string on index saved in indexInArrayOfCharsReplInt
+
+        stringFromArrayOfCharsReplInt = arrayOfCharsReplInt[indexInArrayOfCharsReplInt]; // string on index indexInArrayOfCharsReplInt saved in stringFromArrayOfCharsReplInt
         // printf("str in arrayOfCharsReplInt: %d\n", indexInArrayOfCharsReplInt);
 
         if (find == strlen(sequenceOfNumbers))
         {
-            return find;
+            return 0;
         }
         for (int counterCharsReplInt = 0; counterCharsReplInt < strlen(stringFromArrayOfCharsReplInt); counterCharsReplInt++) // looping through string in arrayOfCharsReplInt
         {
 
-            if (stringFromArrayOfCharsReplInt[counterCharsReplInt] == costumerName[counterCostumerName])
+            if (stringFromArrayOfCharsReplInt[counterCharsReplInt] == costumerName[counterCostumerName]) // checking if character is in costumer name
             {
                 // printf("str in arrayOfCharsReplInt %c ==  costumerName %c\n", stringFromArrayOfCharsReplInt[counterCharsReplInt], costumerName[counterCostumerName]);
                 // printf("costumerName %s\n", costumerName);
@@ -139,8 +141,7 @@ int findByString(char *sequenceOfNumbers, char *costumerName) // function that t
             }
             else
             {
-
-                if (find > 0 && counterCharsReplInt == (strlen(stringFromArrayOfCharsReplInt) - 1))
+                if (find > 0 && counterCharsReplInt == (strlen(stringFromArrayOfCharsReplInt) - 1)) // checking if next character is match if not reset
                 {
                     find = 0;
                     counterSeqOFNum = 0;
@@ -149,10 +150,10 @@ int findByString(char *sequenceOfNumbers, char *costumerName) // function that t
         }
     }
 
-    return find;
+    return -1;
 }
 
-char *find(char *sequenceOfNumbers, struct namePlusPhone namePhoneArray[100], int numberOfContacts, char arrayOfIndexes[100], int *indexCounter)
+char *find(char *sequenceOfNumbers, struct namePlusPhone namePhoneArray[100], int numberOfContacts, char arrayForIndexes[200], int *indexCounter) // function that goes through all the contacts and passes them to the findByNumber and findByString functions and tracks when the functions find match
 {
 
     int counter = 0;
@@ -163,43 +164,83 @@ char *find(char *sequenceOfNumbers, struct namePlusPhone namePhoneArray[100], in
         // printf("find num: %s\n", namePhoneArray[i].phone);
         numberOfFindsInNum = findByNumber(sequenceOfNumbers, namePhoneArray[i].phone);
         numberOfFindsInStr = findByString(sequenceOfNumbers, namePhoneArray[i].name);
-        if (numberOfFindsInNum == strlen(sequenceOfNumbers) || numberOfFindsInStr == strlen(sequenceOfNumbers))
+        if (!numberOfFindsInNum || !numberOfFindsInStr) // checking if aether of the functions found a match
         {
-            //printf("find index: %d\n", namePhoneArray[i].index);
-            arrayOfIndexes[counter] = namePhoneArray[i].index;
+            // printf("find index: %d\n", namePhoneArray[i].index);
+            arrayForIndexes[counter] = namePhoneArray[i].index; // saving index of found contact to an array
             counter++;
         }
     }
-    *indexCounter = counter;
-    return arrayOfIndexes;
+
+    *indexCounter = counter; // saving number of found contacts
+    return arrayForIndexes;
+}
+
+int controlOfInput(char *phoneNumberSequence) // function that check if the input is a number
+{
+    for (int index = 0; index < strlen(phoneNumberSequence); index++)
+    {
+        if (phoneNumberSequence[index] < 48 || phoneNumberSequence[index] > 57)
+        {
+            printf("As an argument please enter number or numbers for example 11 or 602!");
+            return 1;
+        }
+    }
+    return 0;
 }
 
 int main(int argc, char *argv[])
 {
     char c;
     int numberOfContacts;
-    int arrayOfIndexesCounter = 0;
+    int arrayOfIndexesCounter;
     int *indexCounter;
     char *phoneNumberSequence = argv[1];
     char *arrayOfIndexes;
-    char arrayForIndexes[84];
-    struct namePlusPhone namePhoneArray[100];
-    numberOfContacts = readFile(namePhoneArray);
-    arrayOfIndexes = find(phoneNumberSequence, namePhoneArray, numberOfContacts, arrayForIndexes, indexCounter);
+    char arrayForIndexes[200];
+    struct namePlusPhone namePhoneArray[100]; // making array of structures
+    int error = 0;
+    *indexCounter = 0;
 
-    for (int y = 0; y < numberOfContacts; ++y)
+    numberOfContacts = readFile(namePhoneArray);
+
+    if (argc > 1) // check if the program gets an argument
     {
-        for (int i = 0; i < *indexCounter; ++i)
+        error = controlOfInput(phoneNumberSequence);
+
+        if (error) // checking if function controlOfInput returns an error
         {
-            // printf("%d\n", *indexCounter);
-            // printf("%d == %d\n", namePhoneArray[y].index, arrayOfIndexes[i]);
-            if (arrayOfIndexes[i] == namePhoneArray[y].index)
+            return 1;
+        }
+
+        arrayOfIndexes = find(phoneNumberSequence, namePhoneArray, numberOfContacts, arrayForIndexes, indexCounter);
+
+        if (*indexCounter != 0) // checking if there is a found contact
+        {
+            for (int y = 0; y < numberOfContacts; ++y) // printing a result
             {
-                printf("Name: %s\n", namePhoneArray[y].name);
-                printf("Phone: %s\n", namePhoneArray[y].phone);
-                printf("Index: %d\n", namePhoneArray[y].index);
+                for (int i = 0; i < *indexCounter; ++i)
+                {
+                    // printf("%d\n", *indexCounter);
+                    // printf("%d == %d\n", namePhoneArray[y].index, arrayOfIndexes[i]);
+                    if (arrayOfIndexes[i] == namePhoneArray[y].index)
+                    {
+                        printf("%s, %s\n", namePhoneArray[y].name, namePhoneArray[y].phone);
+                    }
+                }
             }
         }
+        else
+        {
+            printf("Contact(s) not found");
+        }
+
+        return 0;
+    }
+
+    for (int y = 0; y < numberOfContacts; ++y) // printing full list of contacts because the program did't get an argument
+    {
+        printf("%s, %s\n", namePhoneArray[y].name, namePhoneArray[y].phone);
     }
     return 0;
 }
